19e1a28de55cc459107d03a987c3b5d06c1a5bc4
Kyle Trusler <KTrusler>
2019-11-08 14:07:46 -0500

    initial commit

---
+++ ActiveXExample/FileSaver.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+using System.IO;
+using System;
+
+namespace ActiveXExample
+{
+    [ComVisible(true)]
+    [ProgId("ActiveXExample")]
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    public class FileSaver : IObjectSafetyImpl
+    {
+        [ComVisible(true)]
+        public bool Exists(string path)
+        {
+            try
+            {
+                return Directory.Exists(path); //
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error thrown on lookup of path {path}\n{e.Message}\n{e.StackTrace}");
+                return false;
+            }
+        }
+    }
+}
